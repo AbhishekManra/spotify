442dc46c0cdc97465bec7d8391787f30a0e7073c
AbhishekManra <abhishek.manral2021@vitbhopal.ac.in>
2023-01-30 02:04:48 +0530

    Final-Commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
 // modules which we installed for our passport framework
 const session = require("express-session");
 const passport = require("passport");
@@ -24,7 +25,7 @@ app.use(session({
 
 app.use(passport.initialize()); // initalizing passport
 app.use(passport.session()); // launching the session
-
+mongoose.set("strictQuery", false);
 mongoose.connect('mongodb://127.0.0.1:27017/AuthurizeDB');
 
 const userSchema = new mongoose.Schema({
@@ -72,18 +73,91 @@ app.get('/auth/google/secrets',
   passport.authenticate('google', { failureRedirect: "/" }),
   function(req, res) {
     // after a sucessful authentication we will redirect our user to the secrets page.
-    res.redirect("/sucess");
+    res.redirect("/spotify.");
   });
 
+app.get("/register",(req,res)=>{
+  res.render("register")
+})
+
 app.get("/",(req,res)=>{
-    res.redirect('/login')
+  res.render("register")
 })
 
 app.get("/login",(req,res)=>{
     res.render("login")
 })
-app.get("/sucess",(req,res)=>{
-    res.render("index")
+app.get("/spotify.",(req,res)=>{
+  res.render("index")
+})
+const isAuth = (req,res,next)=>{
+  if(req.session.isAuth){
+      next()
+  }else{
+      res.redirect("/login");
+  }
+}
+app.get("/spotify",isAuth,(req,res)=>{
+  res.render("index")
+})
+
+app.get("/logout",(req,res)=>{
+  req.session.destroy((err)=>{
+      if(err) throw err;
+      res.redirect("/");
+  });
+});
+
+// post route
+
+app.post("/login",(req,res)=>{
+  const {username,password} = req.body;
+
+    User.findOne({email : username},(err,data)=>{
+
+      
+      if(!data){
+        return res.redirect('/login');
+      }
+      
+      bcrypt.compare(password, data.password, function(err, result) {
+        
+        if(result === false){
+          return res.redirect('/login')
+        }
+        req.session.isAuth = true;
+        res.redirect('/spotify');
+      });
+    });
+    })
+    
+    app.post("/register",(req,res)=>{
+      
+  const {email ,password} = req.body;
+    
+    User.findOne({email : email},(err,data)=>{
+      if(data){
+        return res.redirect('/register');
+      }
+      else{
+        bcrypt.hash(password, 12, (err, hash)=> {
+          
+          if(err){
+            return console.log(err);
+          }
+          const newone = new User({
+            email : email,
+            password : hash
+          })
+          
+          newone.save();
+          
+          res.redirect('/login')
+        });
+      }
+    });
+
+    
 })
 
 app.listen(3000 , ()=>{
